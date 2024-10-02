c40fee628021b26b176ea82fbc27dca3f08f77dd
trishantjain <trishantjain8888@gmail.com>
2024-10-03 00:47:08 +0530

    Started learning typeScript and added first file

---
+++ person.ts
@@ -0,0 +1,32 @@
+type person = {
+    name: string,
+    age: number,
+    isStudent: boolean,
+    address: {
+        street: string,
+        city: string,
+        country: string
+    }
+}
+
+let person1: person = {
+    name: 'John',
+    age: 15,
+    isStudent: true,
+    address: {
+        street: "Johnson",
+        city: "New York",
+        country: "United States"
+    }
+}
+
+let person2: person = {
+    name: 'Trishant',
+    age: 22,
+    isStudent: true,
+    address: {
+        street: "Sec 19",
+        city: "Faridabad",
+        country: "India"
+    }
+}
